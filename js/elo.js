f3cbc371762d68f3c7ba9795cd40422442063eaa
TeamItaliaDoc <sandrotorri@webmail.it>
2019-06-29 11:35:33 +0200

    elo

--- js/elo.js
+++ js/elo.js
@@ -1,5 +1,6 @@
 var elo = [];
-
+var index = 0;
+var totGiocatori = 0;
 
 function showElo() {
     //Inizializzo array
@@ -7,52 +8,53 @@ function showElo() {
     for (var i = 999; i <= 2001; i++) 
        elo[i] = 0;
 
-    //Imposto sincrono   
-    $.ajaxSetup({
-        "async": false
-    });
- 
      //Leggo lista iscritti
     $.getJSON('https://api.chess.com/pub/club/team-italia-doc/members',function(data){
 
-        var index = 0;
         var giocatori = data.all_time;
         giocatori = giocatori.concat(data.weekly);
         giocatori = giocatori.concat(data.monthly);
 
+        totGiocatori = giocatori.length;
+        console.log('totGiocatori: ' + totGiocatori);
         for (var i in giocatori) {
-            console.log('<br>'  + index + ' ' +  giocatori[i].username);
-
-            //if (giocatori[i].username != 'armandoazzola')
-            //    continue;
-
             //Leggo elo
-            $.getJSON('https://api.chess.com/pub/player/' + giocatori[i].username + '/stats',function(dataElo){
-                index++;
-                if (dataElo.chess_daily) {
-                    if (dataElo.chess_daily.last.rating < 1000) {
-                         elo[999]++;
-                    } else if (dataElo.chess_daily.last.rating  > 2000) {
-                         elo[2001]++;
-                    } else  {
-                          elo[dataElo.chess_daily.last.rating]++;
-                    }
-                } else {
-                    console.log('<br>' + index + ' ' + giocatori[i].username + '---- ELO NON PRESENTE')
-                    elo[0]++;
-                }            
-            });
+            getEloUrl('https://api.chess.com/pub/player/' + giocatori[i].username + '/stats');
 
         }
     });
 
-    //Tolgo sincrono
-    $.ajaxSetup({
-        "async": true
+}
+
+function getEloUrl(url)
+{
+    //Eseguo funzione per ricercare un avatar
+    $.getJSON(url,function(dataElo){
+        index++;
+        if (dataElo.chess_daily) {
+            if (dataElo.chess_daily.last.rating < 1000) {
+                 elo[999]++;
+            } else if (dataElo.chess_daily.last.rating  > 2000) {
+                 elo[2001]++;
+            } else  {
+                  elo[dataElo.chess_daily.last.rating]++;
+            }
+        } else {
+            elo[0]++;
+        }            
+
+        console.log('totGiocatori: ' + totGiocatori + ' index: ' + index);
+        if (totGiocatori > index)
+            return;
+
+        //Scrivo risultati   
+        for (var i in elo) {
+            document.write('<br>' + i + ';' + elo[i]);
+        };
+ 
+    }).error(function(jqXhr, textStatus, error) {
+        //è andato in errore ricarico i dati
+        getEloUrl(this.url);    
     });
 
-    //Scrivo risultati   
-    for (var i in elo) {
-        document.write('<br>' + i + ';' + elo[i]);
-    };
 }
